8b3acc59f1c0fa7b4a2095ea306587c644e3a232
yjkim721 <lion721721@gmail.com>
2026-03-06 09:34:58 +0900

    fix: resource path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+  base: '/Mobileweddinginvitation/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
